0c2f6b1d8c5163f305122a4fc57a5d256aa3ca7b
Musa <musaabdullah.hundur@gmail.com>
2022-01-15 15:44:00 +0300

    landmarkbook

---
+++ app/src/main/java/com/musahundur/landmarkbook/DetailsActivity.java
@@ -0,0 +1,21 @@
+package com.musahundur.landmarkbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.musahundur.landmarkbook.databinding.ActivityDetailsBinding;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    private ActivityDetailsBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityDetailsBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+    }
+}
---
+++ app/src/main/java/com/musahundur/landmarkbook/MainActivity.java
@@ -0,0 +1,41 @@
+package com.musahundur.landmarkbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.musahundur.landmarkbook.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Landmark> landmarkArrayList;
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        //Initialize
+        landmarkArrayList = new ArrayList<>();
+
+        //DATA
+        Landmark pisa = new Landmark("Pisa", "Italy", R.drawable.pisa);
+        Landmark eiffel = new Landmark("Eiffel", "France", R.drawable.eiffel);
+        Landmark colosseum = new Landmark("Colosseum", "Italy", R.drawable.colosseum);
+        Landmark londonBridge = new Landmark("London Bridge", "UK", R.drawable.londonbridge);
+
+        //
+        landmarkArrayList.add(pisa);
+        landmarkArrayList.add(eiffel);
+        landmarkArrayList.add(colosseum);
+        landmarkArrayList.add(londonBridge);
+
+    }
+}
